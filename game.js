9a9c69dc68463bad9c70817c3ab4c9f3b0ba0916
HsuBokai <exp2.718281@gmail.com>
2021-03-25 23:46:05 +0800

    show stock price

--- game.js
+++ game.js
@@ -4,17 +4,24 @@ const app = createApp({
 		return {
 			context: null,
 			timeStep: 0,
-			price: 10,
-			stock_buy_price: {
-				A : 10,
-				K : 11,
-				Q : 12
-			},
-			stock_sell_price: {
-				A : 3,
-				K : 4,
-				Q : 5
-			}
+			money: 100,
+			stock_list : [
+				{
+					name : 'TSMC',
+					price : 10,
+					color : '#555555',
+				},
+				{
+					name : 'HTC',
+					price : 10,
+					color : '#5555ff',
+				},
+				{
+					name : 'MTK',
+					price : 10,
+					color : '#55cc55',
+				},
+			],
 		}
 	},
 	created () {
@@ -32,25 +39,26 @@ const app = createApp({
 		nextStep () {
 			var oldStep = this.timeStep;
 			this.timeStep++;
-			for (var i=0; i<1; i++) {
-				var oldPrice = this.price;
+			for (var i=0; i < this.stock_list.length; ++i) {
+				var stock = this.stock_list[i];
+				var oldPrice = stock.price;
 				var r = Math.random();
 				if(r*2 < 1){
-					this.price += 1;
+					stock.price += 1;
 				} else {
-					this.price -= 1;
+					stock.price -= 1;
 				}
 				var newStep = this.timeStep;
-				var newPrice = this.price;
-				this.drawLine(oldStep*2, 400-oldPrice*10, newStep*2, 400-newPrice*10);
+				var newPrice = stock.price;
+				this.drawLine(oldStep*2, 400-oldPrice*10, newStep*2, 400-newPrice*10, stock.color);
 			}
 		},
-		drawLine (x1,y1,x2,y2) {
-			console.log(x1,y1);
+		drawLine (x1,y1,x2,y2,color) {
 			this.context.beginPath();
 			this.context.moveTo(x1, y1);
 			this.context.lineTo(x2, y2);
-			this.context.strokeStyle = 'red';
+			this.context.lineWidth = 2;
+			this.context.strokeStyle = color;
 			this.context.stroke();
 		},
 	},
