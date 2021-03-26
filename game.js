d99f58bfa9548f6bc7a2063de96ae9a28e063865
HsuBokai <exp2.718281@gmail.com>
2021-03-27 01:13:48 +0800

    show table

--- game.js
+++ game.js
@@ -22,6 +22,8 @@ const app = createApp({
 					color : '#55cc55',
 				},
 			],
+			hold_list : [],
+			asset: 100,
 		}
 	},
 	created () {
@@ -30,6 +32,8 @@ const app = createApp({
 	mounted () {
 		console.log('mounted');
 		var c = document.getElementById("myCanvas");
+		c.width = 1000;
+		c.height = 400;
 		this.context = c.getContext('2d');
 	},
 	unmounted () {
@@ -52,6 +56,8 @@ const app = createApp({
 				var newPrice = stock.price;
 				this.drawLine(oldStep*2, 400-oldPrice*10, newStep*2, 400-newPrice*10, stock.color);
 			}
+			this.updateAsset();
+			this.updateProfit();
 		},
 		drawLine (x1,y1,x2,y2,color) {
 			this.context.beginPath();
@@ -60,6 +66,98 @@ const app = createApp({
 			this.context.lineWidth = 2;
 			this.context.strokeStyle = color;
 			this.context.stroke();
+		},
+		updateProfit () {
+			for (var index = 0; index < this.hold_list.length; ++index) {
+				for (var i=0; i < this.stock_list.length; ++i) {
+					if (this.hold_list[index].name === this.stock_list[i].name) {
+						var p = this.stock_list[i].price;
+						this.hold_list[index].profit = this.getProfit(index, p);
+					}
+				}
+			}
+		},
+		updateAsset () {
+			var a = this.money;
+			for (var index = 0; index < this.hold_list.length; ++index) {
+				for (var i=0; i < this.stock_list.length; ++i) {
+					if (this.hold_list[index].name === this.stock_list[i].name) {
+						var p = this.stock_list[i].price;
+						a += this.hold_list[index].count * p;
+						break;
+					}
+				}
+			}
+			this.asset = a;
+		},
+		getProfit (index, p) {
+			var hold = this.hold_list[index];
+			return (hold.count * p) - hold.cost_sum;
+		},
+		getCostAvg (index) {
+			var hold = this.hold_list[index];
+			if (0 === hold.count) {
+				return 0;
+			} else {
+				return hold.cost_sum / hold.count;
+			}
+		},
+		doBuy (stock_name) {
+			for (var i=0; i < this.stock_list.length; ++i) {
+				if (stock_name === this.stock_list[i].name) {
+					var p = this.stock_list[i].price;
+					if (this.money < p) {
+						return;
+					}
+					break;
+				}
+			}
+			var index = 0;
+			for (; index < this.hold_list.length; ++index) {
+				if (stock_name === this.hold_list[index].name) {
+					break;
+				}
+			}
+			if (index === this.hold_list.length) {
+				this.hold_list.push({});
+				this.hold_list[index].name = stock_name;
+				this.hold_list[index].count = 0;
+				this.hold_list[index].cost_sum = 0;
+				this.hold_list[index].cost_avg = 0;
+				this.hold_list[index].profit = 0;
+			}
+			this.money -= p;
+			this.hold_list[index].count += 1;
+			this.hold_list[index].cost_sum += p;
+			this.hold_list[index].cost_avg = this.getCostAvg(index);
+			this.hold_list[index].profit = this.getProfit(index, p);
+			this.updateAsset();
+		},
+		doSell (stock_name) {
+			for (var i=0; i < this.stock_list.length; ++i) {
+				if (stock_name === this.stock_list[i].name) {
+					var p = this.stock_list[i].price;
+					break;
+				}
+			}
+			var index = 0;
+			for (; index < this.hold_list.length; ++index) {
+				if (stock_name === this.hold_list[index].name) {
+					break;
+				}
+			}
+			if (index === this.hold_list.length) {
+				return;
+			}
+			if (0 === this.hold_list[index].count) {
+				return;
+			}
+			this.money += (p-1);
+			this.hold_list[index].count -= 1;
+			this.hold_list[index].cost_sum -= (p-1);
+			this.hold_list[index].cost_avg = this.getCostAvg(index);
+			this.hold_list[index].profit = this.getProfit(index, p);
+			this.updateAsset();
 		},
 	},
 });
